b8976cfab0f3947728b4649b72e5b639ad4da034
chenyuen0103 <chenyuen0103@berkeley.edu>
2025-11-05 12:22:17 -0600

    try vllm

--- experiments/query_vllm.py
+++ experiments/query_vllm.py
@@ -369,7 +369,7 @@ def main():
     )
     ap.add_argument(
         "--csv",
-        default="/home/yuen_chen/ENCO/experiments/out/cancer/prompts_obs200_int3_shuf5_anon.csv",
+        default="../out/cancer/prompts_obs200_int3_shuf5_anon.csv",
         help="Path to the input CSV produced by your generator."
     )
     ap.add_argument(
