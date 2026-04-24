eb84d13ecc01f814fd057534328fa038777f2289
Yuen Chen <yuenc2@illinois.edu>
2026-04-24 10:18:12 -0500

    fix path in clean

--- experiments/clean_responses.sh
+++ experiments/clean_responses.sh
@@ -1,7 +1,7 @@
-mkdir -p responses_old/sachs
-rm  experiments/responses/sachs/*.json
-rm  experiments/responses/sachs/*.pdf
-mv experiments/responses/sachs/*grpo* responses_old/sachs/
+mkdir -p experiments/responses_old/sachs
+rm experiments/responses/sachs/*.json
+rm experiments/responses/sachs/*.pdf
+mv experiments/responses/sachs/*grpo* experiments/responses_old/sachs/
 mv experiments/responses/sachs/*stage*  experiments/responses_old/sachs/
 mv experiments/responses/sachs/*checkpoint-4*  experiments/responses_old/sachs/
 mv experiments/responses/sachs/*sft*  experiments/responses_old/sachs/
